2b2eedd30452004bbb4da01e5628af247fa6d77b
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-04-15 10:30:58 +0200

    chore: apply clippy auto-fixes to e2e tests for idempotency

--- packages/csharp/Kreuzcrawl/NativeMethods.cs
+++ packages/csharp/Kreuzcrawl/NativeMethods.cs
@@ -16,21 +16,6 @@ internal static partial class NativeMethods
     [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_crawl_config_free")]
     internal static extern void CrawlConfigFree(IntPtr ptr);
 
-    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_crawl_config_to_json")]
-    internal static extern IntPtr CrawlConfigToJson(IntPtr ptr);
-
-    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_scrape_result_to_json")]
-    internal static extern IntPtr ScrapeResultToJson(IntPtr ptr);
-
-    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_scrape_result_free")]
-    internal static extern void ScrapeResultFree(IntPtr ptr);
-
-    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_map_result_to_json")]
-    internal static extern IntPtr MapResultToJson(IntPtr ptr);
-
-    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_map_result_free")]
-    internal static extern void MapResultFree(IntPtr ptr);
-
     [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_crawl_result_to_json")]
     internal static extern IntPtr CrawlResultToJson(IntPtr ptr);
 
@@ -42,6 +27,21 @@ internal static partial class NativeMethods
 
     [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_browser_config_free")]
     internal static extern void BrowserConfigFree(IntPtr ptr);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_map_result_to_json")]
+    internal static extern IntPtr MapResultToJson(IntPtr ptr);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_map_result_free")]
+    internal static extern void MapResultFree(IntPtr ptr);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_scrape_result_to_json")]
+    internal static extern IntPtr ScrapeResultToJson(IntPtr ptr);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_scrape_result_free")]
+    internal static extern void ScrapeResultFree(IntPtr ptr);
+
+    [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_crawl_config_to_json")]
+    internal static extern IntPtr CrawlConfigToJson(IntPtr ptr);
 
     [DllImport(LibName, CallingConvention = CallingConvention.Cdecl, EntryPoint = "kcrawl_create_engine")]
     internal static extern IntPtr CreateEngine(
